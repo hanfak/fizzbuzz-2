779c8e943db4496d6623765c4dd0f943e6491e36
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:08:45 +0000

    returns fizz if number divisble by 3 refactor private method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,14 @@
 class Fizzbuzz
   def game(number)
-    'Fizz' if number % 3 == 0 
+    'Fizz' if divisible_by_3?(number)
+    # What if bigger exp, harder to read and understand
   end
+
+  private
+    # Do not need to test private methods
+    def divisible_by_3?(number)
+      number % 3 == 0
+    end
 end
 
 # Fizzbuzz.new.game(3)
