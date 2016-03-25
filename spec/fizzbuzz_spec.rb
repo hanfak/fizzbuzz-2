feb8c573b7221a6e9632ba403fe1b60107fb4c64
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:33:25 +0000

    added context blocks for tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,21 +4,24 @@ describe Fizzbuzz do
   let (:fizzbuzz) {described_class.new}
 
   describe '#game' do
-    it 'returns Fizz, if divisible by 3' do
-      expect(fizzbuzz.game(3)).to eq 'Fizz'
+    context 'divisible by 3' do
+      it 'returns Fizz' do
+        expect(fizzbuzz.game(3)).to eq 'Fizz'
+      end
+
+      it 'Does not return Fizz' do
+        expect(fizzbuzz.game(8)).not_to eq 'Fizz'
+      end
     end
 
-    it 'Does not return Fizz, if not divisible by 3' do
-      expect(fizzbuzz.game(8)).not_to eq 'Fizz'
-    end
+    context 'divisible by 5' do
+      it 'returns Buzz' do
+        expect(fizzbuzz.game(5)).to eq 'Buzz'
+      end
 
-    it 'returns Buzz, if divisible by 5' do
-      expect(fizzbuzz.game(5)).to eq 'Buzz'
+      it 'Does not return Buzz' do
+        expect(fizzbuzz.game(17)).not_to eq 'Buzz'
+      end
     end
-
-    it 'Does not return Buzz, if not divisible by 5' do
-      expect(fizzbuzz.game(17)).not_to eq 'Buzz'
-    end
-
   end
 end
