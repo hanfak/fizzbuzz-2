3fa620a8fed4c967c6f0ff2a30ca0b939f1ac09b
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:06:14 +0000

    returns fizz if number divisble by 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 class Fizzbuzz
   def game(number)
+    'Fizz' if number % 3 == 0 
   end
 end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,5 +3,14 @@ require 'fizzbuzz'
 describe Fizzbuzz do
   it {is_expected.to respond_to(:game).with(1).argument}
 
+  describe '#game' do
+    it 'returns Fizz, if divisible by 3' do
+      expect(Fizzbuzz.new.game(3)).to eq 'Fizz'
+    end
+
+    it 'Does not return Fizz, if not divisible by 3' do
+      expect(Fizzbuzz.new.game(8)).not_to eq 'Fizz'
+    end
+  end
 
 end
