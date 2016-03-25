1133c390aca1130aa50f50bda14bcf60afe233b8
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:29:43 +0000

    used described_class to remove objects in each test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,22 +1,23 @@
 require 'fizzbuzz'
 
 describe Fizzbuzz do
+  let (:fizzbuzz) {described_class.new}
 
   describe '#game' do
     it 'returns Fizz, if divisible by 3' do
-      expect(Fizzbuzz.new.game(3)).to eq 'Fizz'
+      expect(fizzbuzz.game(3)).to eq 'Fizz'
     end
 
     it 'Does not return Fizz, if not divisible by 3' do
-      expect(Fizzbuzz.new.game(8)).not_to eq 'Fizz'
+      expect(fizzbuzz.game(8)).not_to eq 'Fizz'
     end
 
     it 'returns Buzz, if divisible by 5' do
-      expect(Fizzbuzz.new.game(5)).to eq 'Buzz'
+      expect(fizzbuzz.game(5)).to eq 'Buzz'
     end
 
     it 'Does not return Buzz, if not divisible by 5' do
-      expect(Fizzbuzz.new.game(17)).not_to eq 'Buzz'
+      expect(fizzbuzz.game(17)).not_to eq 'Buzz'
     end
 
   end
