79a0ccf9f45672c81022f11e9ea76ceed51696ca
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:57:20 +0000

    refactored divisble methods to general method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -9,15 +9,19 @@ class Fizzbuzz
   private
 
     def divisible_by_3?(number)
-      number % 3 == 0
+      divisbile_by?(number,3)
     end
 
     def divisible_by_5?(number)
-      number % 5 == 0
+      divisbile_by?(number,5)
     end
 
     def divisible_by_15?(number)
-      number % 15 == 0
+      divisbile_by?(number,15)
+    end
+    # unnecessary for private method to encapsulate further, but need to if public
+    def divisbile_by?(number,divisor)
+      number%divisor == 0
     end
 end
 
