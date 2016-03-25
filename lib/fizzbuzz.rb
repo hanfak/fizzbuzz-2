ee297db472b23041ed6720640def40af91a3aa92
Hanif Fakira <fakira.work@gmail.com>
2016-03-25 11:34:43 +0000

    refactored divisible by 5 method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,17 @@
 class Fizzbuzz
   def game(number)
     return 'Fizz' if divisible_by_3?(number)
-    'Buzz' if number % 5 == 0
+    'Buzz' if divisible_by_5?(number)
   end
 
   private
 
     def divisible_by_3?(number)
       number % 3 == 0
+    end
+
+    def divisible_by_5?(number)
+      number % 5 == 0
     end
 end
 
