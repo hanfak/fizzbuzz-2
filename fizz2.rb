eaa81a4cfa4b1bc304bb223aaf7531d71284949c
Hanif Fakira <fakira.work@gmail.com>
2016-04-08 11:45:30 +0100

    fizzbuzz with separate classes

--- fizz2.rb
+++ fizz2.rb
@@ -1,14 +1,14 @@
 class Fizzbuzz
   def game(number)
-    return 'FizzBuzz' if divisible_by?(15)
-    return 'Fizz' if divisible_by?(3)
-    return 'Buzz' if divisible_by?(5)
+    return 'FizzBuzz' if number.divisible_by?(15)
+    return 'Fizz' if number.divisible_by?(3)
+    return 'Buzz' if number.divisible_by?(5)
     number
   end
 end
 
-def Fixnum
-  def divisbile_by?(divisor)
+class Fixnum
+  def divisible_by?(divisor)
     self%divisor == 0
   end
 end
